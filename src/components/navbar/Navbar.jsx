3a589246d10f1a5a9af7cd31010ea39f3b08ce26
Arthur van der Elst <arthurvde@hotmail.fr>
2023-02-17 15:17:12 +0100

    header fully done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
         </div>
         <div className="gpt3__navbar-links_container">
           <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT3?</a></p>
+          <p><a href="#wgpt3">What is GPT?</a></p>
           <p><a href="#possibility">Open AI</a></p>
           <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
@@ -32,7 +32,7 @@ const Navbar = () => {
         <div className="gpt3__navbar-menu_container scale-up-center">
           <div className="gpt3__navbar-menu_container-links">
             <p><a href="#home">Home</a></p>
-            <p><a href="#wgpt3">What is GPT3?</a></p>
+            <p><a href="#wgpt3">What is GPT?</a></p>
             <p><a href="#possibility">Open AI</a></p>
             <p><a href="#features">Case Studies</a></p>
             <p><a href="#blog">Library</a></p>
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 import './header.css';
 
-const Header = () => {
-	return (
-		<div>
-			Header
-		</div>
-	)
-}
+const Header = () => (
+  <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+      <h1 className="gradient__text">A thrilling engaging, innovative, corporate title</h1>
+      <p>Some cool corporate text : Revolutionize the way you work with our cutting-edge tech. Our startup provides innovative solutions to streamline your business and increase efficiency.</p>
 
-export default Header
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+
+      <div className="gpt3__header-content__people">
+        <img src={people} alt='people'/>
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
+    </div>
+
+    <div className="gpt3__header-image">
+      <img src={ai} alt='ai'/>
+    </div>
+  </div>
+);
+
+export default Header;
